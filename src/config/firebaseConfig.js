42bba4188f9cf1ed6284cdf925cfee73293cc759
Avik Kadakia <avik.kadakia@stonybrook.edu>
2019-11-02 14:25:57 -0400

    Added the database

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,16 +5,17 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-    apiKey: "AIzaSyCJxkqx-6PMJrZ7ACkrgbO55b5wmJdop1Y",
-    authDomain: "todo-rrf-316.firebaseapp.com",
-    databaseURL: "https://todo-rrf-316.firebaseio.com",
-    projectId: "todo-rrf-316",
-    storageBucket: "todo-rrf-316.appspot.com",
-    messagingSenderId: "892398996038",
-    appId: "1:892398996038:web:1fb9157fc6c5d266e01847",
-    measurementId: "G-TEGQB3MZ23"
-};
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyCFha0jCk_GxHo2QosRynGBv49Sj-NSsvM",
+    authDomain: "todo-rff-316.firebaseapp.com",
+    databaseURL: "https://todo-rff-316.firebaseio.com",
+    projectId: "todo-rff-316",
+    storageBucket: "todo-rff-316.appspot.com",
+    messagingSenderId: "241005177864",
+    appId: "1:241005177864:web:bcd1ef14c5603220fe35bd",
+    measurementId: "G-1FGWLMW5J7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 // NOW THE firebase OBJECT CAN BE CONNECTED TO THE STORE
 export default firebase;
